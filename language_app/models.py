3fdfde69795c1957c577c17e15b147d044caff10
Eren Özcan <qwerrty38@outlook.com>
2024-05-11 23:41:43 +0300

    UserWords tablosu eklendi. Words tablosuna img column u eklnedi ve User tablosu düzenlendi

--- language_app/models.py
+++ language_app/models.py
@@ -6,10 +6,19 @@ class User(models.Model):
     last_name = models.CharField(max_length=30)
     email = models.EmailField()
     password = models.CharField(max_length=30)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Words(models.Model):
     english = models.CharField(max_length=30)
     turkish = models.CharField(max_length=30)
+    img = models.ImageField(upload_to='images/')
+
+
+class UserWords(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    word = models.ForeignKey(Words, on_delete=models.CASCADE)
+    is_learned = models.BooleanField(default=False)
+    corect_count = models.IntegerField(default=0)
+
+    class Meta:
+        unique_together = ('user', 'word')
