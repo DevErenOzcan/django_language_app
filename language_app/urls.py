ea226c34ce1c90122c02722b29a018dc26c8c0e2
Eren Özcan <qwerrty38@outlook.com>
2024-05-18 18:45:38 +0300

    quiz kısmının algoritması oluşturuldu language_app/quiz/ sayfasında consola yazdırılıyor

--- language_app/urls.py
+++ language_app/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home_view, name='home'),
+    path('add_word/', views.add_word_view, name='add_word'),
+    path('quiz/', views.quiz_view, name='quiz'),
 ]
--- language_app/views.py
+++ language_app/views.py
@@ -1,10 +1,15 @@
+import json
+import random
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
+from language_app.forms import WordsForm
+from .models import UserWords, Words
 
 
 @login_required(login_url=reverse_lazy("login"))
-def home(request):
+def home_view(request):
     if request.method == "POST":
         pass
     else:
@@ -12,6 +17,45 @@ def home(request):
 
 
 @login_required(login_url=reverse_lazy("login"))
-def test(request):
-    return render(request, "test.html")
+def quiz_view(request):
+    if request.method == "POST":
+        words = []
+        word_count = 0
+        unique_words = set()  # Using a set to keep track of unique words
+        all_words = list(Words.objects.all())  # Fetch all words from the database
+        while word_count < 6 and len(unique_words) < len(all_words):
+            random_word = random.choice(all_words)
+            if random_word.english not in unique_words:
+                word_count += 1
+                unique_words.add(random_word.english)
+                choises = [random_word.turkish]
+                selected_choises = random.sample(all_words, 4)
+                for selected_choise in selected_choises:
+                    choises.append(selected_choise.turkish)
 
+                random.shuffle(choises)
+                question_data = {
+                    'question': random_word.english,
+                    # TODO: question resmi eklenecek
+                    'choices0': choises[0],
+                    'choices1': choises[1],
+                    'choices2': choises[2],
+                    'choices3': choises[3],
+                    'choices4': choises[4],
+                }
+                words.append(question_data)
+        return HttpResponse(json.dumps(words), content_type="application/json")
+    else:
+        return render(request, "quiz.html")
+
+
+@login_required(login_url=reverse_lazy("login"))
+def add_word_view(request):
+    if request.method == 'POST':
+        form = WordsForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('word_list')
+    else:
+        form = WordsForm()
+    return render(request, 'add_word.html', {'form': form})
