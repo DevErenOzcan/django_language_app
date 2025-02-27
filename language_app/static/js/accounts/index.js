1dfe3cdb6867a137c25045da6c3403363ae3288f
Eren Özcan <dev.eren.ozcan@proton.me>
2025-02-27 17:07:27 +0300

    port is 8000 now

--- language_app/static/js/accounts/index.js
+++ language_app/static/js/accounts/index.js
@@ -1,7 +1,7 @@
 document.getElementById("login_button").addEventListener("click", function() {
-    window.location.href = "http://localhost:8080/login/"; // Buraya gitmek istediğiniz URL'yi yazın
+    window.location.href = "http://localhost:8000/login/"; // Buraya gitmek istediğiniz URL'yi yazın
 });
 
 document.getElementById("register_button").addEventListener("click", function() {
-    window.location.href = "http://localhost:8080/register"; // Buraya gitmek istediğiniz URL'yi yazın
+    window.location.href = "http://localhost:8000/register"; // Buraya gitmek istediğiniz URL'yi yazın
 });
